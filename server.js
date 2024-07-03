463a036176aee88df41a1e617ba78e090648ea46
Matheus Militão <42574195+Militao36@users.noreply.github.com>
2024-07-03 11:31:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ server.use(express.urlencoded({ extended: true }))
 server.use(cors())
 
 server.all('*', async (req, res) => {
-  await database.table('webhook')
-    .insert({
-      request: {
-        body: req.body,
-        headers: req.headers,
-        params: req.params,
-        query: req.query,
-      }
-    })
+ 
   return res.status(200).json({})
 })
 
